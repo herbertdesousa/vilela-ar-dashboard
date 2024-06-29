163bd6867103b9eb872b70de59663008243c343c
herbertdesousa <sousaherbert138@gmail.com>
2024-06-29 15:25:48 -0300

    feat(DocumentEditorSideMenuBlock): standarize header

--- src/modules/Document/DocumentEditor/DocumentEditorSideMenu/DocumentEditorSideMenuBlock/index.tsx
+++ src/modules/Document/DocumentEditor/DocumentEditorSideMenu/DocumentEditorSideMenuBlock/index.tsx
@@ -48,21 +48,17 @@ const DocumentEditorSideMenuBlock: React.FC = () => {
   }
   return (
     <section
-      className="min-h-full py-16 px-10 overflow-x-scroll no-scroll"
+      className="flex flex-col gap-y-8 min-h-full overflow-x-scroll no-scroll"
       style={{ width: 512 }}
     >
-      <div className="flex items-center justify-between mb-10">
-        <div className="flex items-center">
-          <button
-            type="button"
-            onClick={() => router.push('/documents/editor')}
-          >
-            <MdChevronLeft size={24} className="text-accent-6 mr-6" />
-          </button>
-          <h1 className="font-merriweather text-4xl font-bold">
-            {`Bloco ${romanFormat(blockIndex)}`}
-          </h1>
-        </div>
+      <div className="flex items-center justify-between px-10 py-6 border-b border-accent-2">
+        <button type="button" onClick={() => router.push('/documents/editor')}>
+          <MdChevronLeft size={24} className="text-accent-6" />
+        </button>
+
+        <h1 className="font-merriweather text-lg font-bold">
+          {`Bloco ${romanFormat(blockIndex)}`}
+        </h1>
 
         <div className="relative flex justify-end">
           <button
@@ -96,125 +92,127 @@ const DocumentEditorSideMenuBlock: React.FC = () => {
         </div>
       </div>
 
-      <Select
-        name={`layers[${blockIndex}].title`}
-        label="Nome"
-        isRequired
-        placeholder="Nome do bloco"
-        data={{
-          variant: 'single',
-          fetch: async () => {
-            const response = await internalApi.get<{ name: string }[]>(
-              'documents',
-              { params: { type: 'block-name' } },
-            );
-
-            return response.data.map(i => ({ value: i.name }));
-          },
-          onAddFilter: async name => {
-            await internalApi.post(
-              `${internalApi}/documents`,
-              { name },
-              { params: { type: 'block-name' } },
-            );
-          },
-        }}
-      />
-      <TextField
-        name={`layers[${blockIndex}].description`}
-        label="Descrição"
-        type="textarea"
-        placeholder="Descrição"
-        containerClassName="mt-4"
-      />
-      <TextField
-        name={`layers[${blockIndex}].price`}
-        label="Valor (R$)"
-        containerClassName="mt-4"
-        isRequired
-        formatOnChangeText={moneyFormat}
-        placeholder="Valor para este bloco"
-      />
-      <ClosableList title="Materiais" containerClassName="mt-8">
+      <div className="pb-16 px-10">
         <Select
-          name={`layers[${blockIndex}].materials`}
-          label="Materiais"
-          placeholder="Selecione os materiais"
+          name={`layers[${blockIndex}].title`}
+          label="Nome"
+          isRequired
+          placeholder="Nome do bloco"
           data={{
-            variant: 'array',
+            variant: 'single',
             fetch: async () => {
               const response = await internalApi.get<{ name: string }[]>(
                 'documents',
-                { params: { type: 'materials' } },
+                { params: { type: 'block-name' } },
               );
 
               return response.data.map(i => ({ value: i.name }));
             },
             onAddFilter: async name => {
               await internalApi.post(
-                'documents',
+                `${internalApi}/documents`,
                 { name },
-                { params: { type: 'materials' } },
+                { params: { type: 'block-name' } },
               );
             },
           }}
         />
-        <ul className="flex flex-wrap mt-2">
-          {(layers.value[blockIndex] as any).materials.map(item => (
-            <li key={item}>
-              <button
-                type="button"
-                className="flex items-center px-2 py-1 bg-accent-2 rounded mr-2 h-8 mt-2"
-                onClick={() => layers.materials.remove(blockId, item)}
-              >
-                <MdClose size={16} className="mr-2" />
-                {item}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </ClosableList>
-      <ClosableList
-        title="Local Aparelho"
-        containerClassName="mt-8"
-        onAddBlock={() => layers.places.add(blockId)}
-        list={{
-          data: (layers.value[blockIndex] as any).places,
-          renderItem: (item: IDocumentFormDataLayersBlockPlace, idx) => (
-            <ListItem
-              key={item.id}
-              title={`${
-                item.floor && item.room
-                  ? `${item.floor} - ${item.room}`
-                  : `${item.floor}${item.room}`
-              }`}
-              description={`${item.devices.length} Aparelhos`}
-              onClick={() => pushBlockPlacePage(item.id)}
-              rightComponent={{
-                icon: MdMoreVert,
-                dropdown: {
-                  data: [
-                    {
-                      item: 'Duplicar Item',
-                      value: 'duplicate',
-                      onSelect: () => layers.places.duplicate(blockId, item),
-                    },
-                    {
-                      item: <span className="text-red">Remover</span>,
-                      value: 'remove',
-                      onSelect: () => layers.places.remove(blockId, item.id),
-                    },
-                  ],
-                },
-              }}
-              showBottomIndicator={
-                (layers.value[blockIndex] as any).places.length - 1 !== idx
-              }
-              className={idx !== 0 && 'mt-2'}
-            />
-          ),
-        }}
-      />
+        <TextField
+          name={`layers[${blockIndex}].description`}
+          label="Descrição"
+          type="textarea"
+          placeholder="Descrição"
+          containerClassName="mt-4"
+        />
+        <TextField
+          name={`layers[${blockIndex}].price`}
+          label="Valor (R$)"
+          containerClassName="mt-4"
+          isRequired
+          formatOnChangeText={moneyFormat}
+          placeholder="Valor para este bloco"
+        />
+        <ClosableList title="Materiais" containerClassName="mt-8">
+          <Select
+            name={`layers[${blockIndex}].materials`}
+            label="Materiais"
+            placeholder="Selecione os materiais"
+            data={{
+              variant: 'array',
+              fetch: async () => {
+                const response = await internalApi.get<{ name: string }[]>(
+                  'documents',
+                  { params: { type: 'materials' } },
+                );
+
+                return response.data.map(i => ({ value: i.name }));
+              },
+              onAddFilter: async name => {
+                await internalApi.post(
+                  'documents',
+                  { name },
+                  { params: { type: 'materials' } },
+                );
+              },
+            }}
+          />
+          <ul className="flex flex-wrap mt-2">
+            {(layers.value[blockIndex] as any).materials.map(item => (
+              <li key={item}>
+                <button
+                  type="button"
+                  className="flex items-center px-2 py-1 bg-accent-2 rounded mr-2 h-8 mt-2"
+                  onClick={() => layers.materials.remove(blockId, item)}
+                >
+                  <MdClose size={16} className="mr-2" />
+                  {item}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </ClosableList>
+        <ClosableList
+          title="Local Aparelho"
+          containerClassName="mt-8"
+          onAddBlock={() => layers.places.add(blockId)}
+          list={{
+            data: (layers.value[blockIndex] as any).places,
+            renderItem: (item: IDocumentFormDataLayersBlockPlace, idx) => (
+              <ListItem
+                key={item.id}
+                title={`${
+                  item.floor && item.room
+                    ? `${item.floor} - ${item.room}`
+                    : `${item.floor}${item.room}`
+                }`}
+                description={`${item.devices.length} Aparelhos`}
+                onClick={() => pushBlockPlacePage(item.id)}
+                rightComponent={{
+                  icon: MdMoreVert,
+                  dropdown: {
+                    data: [
+                      {
+                        item: 'Duplicar Item',
+                        value: 'duplicate',
+                        onSelect: () => layers.places.duplicate(blockId, item),
+                      },
+                      {
+                        item: <span className="text-red">Remover</span>,
+                        value: 'remove',
+                        onSelect: () => layers.places.remove(blockId, item.id),
+                      },
+                    ],
+                  },
+                }}
+                showBottomIndicator={
+                  (layers.value[blockIndex] as any).places.length - 1 !== idx
+                }
+                className={idx !== 0 && 'mt-2'}
+              />
+            ),
+          }}
+        />
+      </div>
     </section>
   );
 };
