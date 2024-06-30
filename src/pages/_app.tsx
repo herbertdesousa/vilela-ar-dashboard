fa0d990f9581fe4c4b0a14fc231f3ab40f16e10e
herbertdesousa <sousaherbert138@gmail.com>
2024-06-30 16:57:33 -0300

    feat(_app): global head title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,8 @@ const App = ({ Component, pageProps }: any) => {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Merriweather:wght@400;700&display=swap"
           rel="stylesheet"
         />
+
+        <title>Vilela Ar</title>
       </Head>
 
       <SWRConfig>
